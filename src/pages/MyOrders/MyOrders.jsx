eb52c4a738d9b8795b44094399b5268091240349
rasel <rasel708211@gmail.com>
2025-12-08 20:21:18 +0600

    Create MyOrders API route and enable order status update on Cancel button click

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -4,38 +4,20 @@ import { useQuery } from "@tanstack/react-query";
 import { UserAuthContext } from "../../ContextAPI/AuthContext";
 import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
+import { toast } from "react-toastify";
+import Loading from "../../Components/Loading";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
 
 const paginationModel = { page: 0, pageSize: 5 };
 
@@ -43,7 +25,11 @@ const MyOrders = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useContext(UserAuthContext);
 
-  const { data: myorders = [], isLoading } = useQuery({
+  const {
+    data: myorders = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["myorder", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/book-order/${user?.email}`);
@@ -51,7 +37,99 @@ const MyOrders = () => {
     },
   });
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "title", headerName: "Book Title", width: 280 },
+
+    {
+      field: "orderDate",
+      headerName: "Order Date",
+      width: 250,
+      renderCell: (params) => (
+        <span>{new Date(params.value).toLocaleString()}</span>
+      ),
+    },
+    {
+      field: "status",
+      headerName: "status",
+      width: 150,
+      renderCell: (params) => (
+        <span
+          className={`${
+            params.value === "cancel" ? "text-red-500" : "text-green-500"
+          }`}
+        >
+          {params.value}
+        </span>
+      ),
+    },
+    {
+      field: "paymentStatus",
+      headerName: "payment status",
+      width: 200,
+    },
+
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 300,
+      renderCell: (params) => (
+        <>
+          <>
+            <button
+              disabled={params.row.status === "cancel"}
+              className="btn btn-error mx-3"
+              onClick={() => handleCancel(params.row)}
+            >
+              Cancel
+            </button>
+            <button
+              disabled={params.row.status === "cancel"}
+              className="btn btn-primary"
+              onClick={() => handlePayNow(params.row)}
+            >
+              Pay Now
+            </button>
+          </>
+        </>
+      ),
+    },
+  ];
+
   console.log(myorders);
+
+  const rows = myorders.map((item, index) => ({
+    id: index + 1,
+    title: item.book_Name || "No Book",
+    orderDate: item.createdAt,
+    status: item.status,
+    paymentStatus: item.paymentStatus,
+    bookId: item.bookId,
+    orderId: item._id,
+  }));
+
+  const handleCancel = (orderItem) => {
+    console.log("Cancel Order:", orderItem);
+    axiosSecure
+      .patch(`/book-order-cancel/${orderItem.orderId}`)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          toast.success("Oreder Cancel Successfully");
+          refetch();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handlePayNow = (id) => {
+    console.log("Pay Now for:", id);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <div>
@@ -62,7 +140,7 @@ const MyOrders = () => {
           initialState={{ pagination: { paginationModel } }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
-          sx={{ border: 0 }}
+          sx={{ border: 1 }}
         />
       </Paper>
     </div>
