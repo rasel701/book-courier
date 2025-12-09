664d6a25a9f5492bec018404a62fd069455caacf
rasel <rasel708211@gmail.com>
2025-12-09 13:40:05 +0600

    librarian book for fetch for database

--- src/pages/MyBook/MyBook.jsx
+++ src/pages/MyBook/MyBook.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserAuthContext } from "../../ContextAPI/AuthContext";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const MyBook = () => {
+  const { user } = useContext(UserAuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  const { data: librarianBooks = [], isLoading } = useQuery({
+    queryKey: ["librarian-book", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/librarian-book/${user?.email}`);
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
+  console.log(librarianBooks);
+
   return (
     <div>
       <h2>This is a my book route</h2>
