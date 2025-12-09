521919d8d67ca70a85775554c128454e04bdccd5
rasel <rasel708211@gmail.com>
2025-12-09 19:21:38 +0600

    Book status published and unpublished system created

--- src/pages/MyBook/MyBook.jsx
+++ src/pages/MyBook/MyBook.jsx
@@ -2,12 +2,21 @@ import React, { useContext } from "react";
 import { UserAuthContext } from "../../ContextAPI/AuthContext";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { DataGrid } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import { toast } from "react-toastify";
+
+const paginationModel = { page: 0, pageSize: 5 };
 
 const MyBook = () => {
   const { user } = useContext(UserAuthContext);
   const axiosSecure = useAxiosSecure();
 
-  const { data: librarianBooks = [], isLoading } = useQuery({
+  const {
+    data: librarianBooks = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["librarian-book", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/librarian-book/${user?.email}`);
@@ -18,9 +27,88 @@ const MyBook = () => {
 
   console.log(librarianBooks);
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "bookName", headerName: "Book Name", width: 280 },
+
+    {
+      field: "image",
+      headerName: "Book Image",
+      width: 250,
+      renderCell: (params) => (
+        <img
+          src={params.value}
+          className="w-[60px] h-[60px] object-center rounded-full"
+          alt=""
+        />
+      ),
+    },
+
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      width: 200,
+      renderCell: (params) => (
+        <span>{new Date(params.value).toLocaleString()}</span>
+      ),
+    },
+
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 300,
+      renderCell: (params) => (
+        <>
+          <>
+            <button
+              className={`${
+                params.row.status === "published"
+                  ? "bg-red-500 btn"
+                  : "btn bg-blue-500"
+              }`}
+              onClick={() => handleStageChangeBtn(params.row)}
+            >
+              {params.row.status === "published" ? "Unpublished" : "Published"}
+            </button>
+
+            <button className="btn btn-primary mx-2">Edid</button>
+          </>
+        </>
+      ),
+    },
+  ];
+
+  const rows = librarianBooks.map((item, index) => ({
+    id: index + 1,
+    bookName: item.bookName,
+    image: item.image,
+    createdAt: item.createdAt,
+    status: item.status,
+    bookId: item._id,
+  }));
+
+  const handleStageChangeBtn = async (bookItem) => {
+    const res = await axiosSecure.patch(`/books/${bookItem.bookId}`, bookItem);
+    console.log(res.data);
+    if (res.data.modifiedCount > 0) {
+      refetch();
+    }
+  };
+
   return (
     <div>
       <h2>This is a my book route</h2>
+      <Paper sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          rowHeight={80}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          sx={{ border: 1 }}
+        />
+      </Paper>
     </div>
   );
 };
